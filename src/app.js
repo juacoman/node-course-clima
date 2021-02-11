2b37d3b5b6bd07fa9a68e7dff99b4cf24e737601
JoaquinCarvajal <joacarvajalsuazo@gmail.com>
2021-02-11 18:02:31 -0300

    HerokuSETUP

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs  = require('hbs')
 const geoUtil = require('../utils/geocode')
 const forecast = require('../utils/forecast')
 const app = express()
+const port = process.env.PORT || 3000
 
 console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
@@ -93,6 +94,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000')
+app.listen(port, () => {
+    console.log('Server running on port ' + port)
 })
